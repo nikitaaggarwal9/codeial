fa4443e3624f0f4c823dd8963a69e28367bac1dc
Nikita <nikitaaggarwal6600@gmail.com>
2022-11-30 02:45:30 +0530

    sign-up set up done

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
+app.use(express.urlencoded());
 app.use(express.static('./assets'));
+
+app.use(cookieParser());
 
 app.use(expressLayouts);
 
@@ -12,13 +17,13 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 // use express router
 app.use('/', require('./routes'));
 
-// set up the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
 app.listen(port, function(err) {
     if(err) {
